ada57a8180875ce88ceeda018c32faa69aaaf75c
AGKhalil <elghazalykhalil@gmail.com>
2019-02-24 22:16:00 +0000

    Both scripts commented and modified in accordance to the pep8 style guide.

--- grid_world.py
+++ grid_world.py
@@ -1,3 +1,5 @@
+"""Generates grid-world environments.
+"""
 import numpy as np
 import itertools
 import copy
@@ -5,25 +7,43 @@ import random
 import matplotlib.pyplot as plt
 from collections import defaultdict
 
+
 class GridWorld:
 
-    def __init__(self):
+    """Class for grids. Creates intermediate grids between an initial and final
+    grid by removing one structural limit at a time. Provides interface
+    with grid dynamics and structure.
 
+    Attributes:
+        diff_grid (TYPE): grid difference between initial and final grids.
+            Highlights structural barriers
+        final_grid (TYPE): final grid (highest number of barriers)
+        initial_grid (TYPE): initial grid (least number of barriers)
+        list_of_maps (TYPE): list of all maps created; initial, intermediate,
+            final
+        nA (int): number of possible actions
+        nS (TYPE): number of possible states
+        possible_actions (list): possible actions allowed for a grid cell
+        x_lim (int): grid x-length
+        x_start (int): grid possible starting x-coordinate
+        y_lim (int): grid y-length
+        y_start (int): grid possible starting y-coordinate
+    """
+
+    def __init__(self):
+        """Initializes grids from intermediate grid creation and interfaces.
+        """
         self.x_lim = 4
         self.y_lim = 4
         self.nA = 3
         self.nS = self.x_lim * self.y_lim
-
-        # Start position
         self.x_start = 0
         self.y_start = 3
 
         self.reset_state()
 
-        # [U, D, R, L]
         self.possible_actions = [0, 1, 2, 3]
 
-        # (U, D, R, L, T)
         self.initial_grid = {
             (0, 0): {'actions': (1, 0, 1, 0), 'done': False, 'reward': -0.1},
             (0, 1): {'actions': (1, 1, 1, 0), 'done': False, 'reward': -0.1},
@@ -43,7 +63,6 @@ class GridWorld:
             (3, 3): {'actions': (0, 1, 0, 0), 'done': True, 'reward': 1},
         }
 
-        # (U, D, R, L, T)
         self.final_grid = {
             (0, 0): {'actions': (1, 0, 1, 0), 'done': False, 'reward': -0.1},
             (0, 1): {'actions': (1, 1, 0, 0), 'done': False, 'reward': -0.1},
@@ -65,22 +84,33 @@ class GridWorld:
 
         self.list_of_maps = [copy.deepcopy(self.final_grid)]
 
-        # self.states = self.all_states()
         self.diff_grid = copy.deepcopy(self.final_grid)
-        self.diff_grid = self.make_diff_grid()
 
     def show_grid(self, c_map):
-        for  y in range(self.y_lim-1, -1, -1):
-            print('-'*self.x_lim*9)
+        """Print a given grid to terminal.
+
+        Args:
+            c_map (TYPE): a given grid
+        """
+        for y in range(self.y_lim - 1, -1, -1):
+            print('-' * self.x_lim * 9)
             for x in range(self.x_lim):
-                direct = ''.join(self.arrow(i, j) for i, j in enumerate(c_map[(x, y)]['actions']))
-                print('{:^7}'.format(direct.replace(" ", "")), "|", end="")
+                direct = ''.join(self.arrow(i, j)
+                                 for i, j in enumerate(
+                    c_map[(x, y)]['actions']))
+                print('{:^7}'.
+                      format(direct.replace(" ", "")), "|", end="")
             print("")
-        print('-'*self.x_lim*9)
+        print('-' * self.x_lim * 9)
 
     def show_policy(self, c_map):
-        for  y in range(self.y_lim-1, -1, -1):
-            print('-'*self.x_lim*9)
+        """Print policy to terminal to a grid.
+
+        Args:
+            c_map (TYPE): a given policy
+        """
+        for y in range(self.y_lim - 1, -1, -1):
+            print('-' * self.x_lim * 9)
             for x in range(self.x_lim):
                 if x == self.x_lim - 1 and y == self.y_lim - 1:
                     direct = ''.join(self.arrow(4))
@@ -88,9 +118,18 @@ class GridWorld:
                     direct = ''.join(self.arrow(c_map.get((x, y), None)))
                 print('{:^7}'.format(direct.replace(" ", "")), "|", end="")
             print("")
-        print('-'*self.x_lim*9)
+        print('-' * self.x_lim * 9)
 
     def arrow(self, direction, val=1):
+        """Converts an action to an arrow for printing to terminal.
+
+        Args:
+            direction (TYPE): direction of action
+            val (int, optional): if action is possible for a cell
+
+        Returns:
+            TYPE: direction representation for printing
+        """
         if val:
             if direction == 0:
                 return '\u2191'
@@ -108,11 +147,18 @@ class GridWorld:
             return ' '
 
     def make_diff_grid(self):
+        """Creates differential grid to highlight structural barriers between
+        the initial and final grids.
+        """
         for key, value in self.diff_grid.items():
-            self.diff_grid[key]['actions'] = np.subtract(self.initial_grid[key]['actions'], value['actions'])
-        return self.diff_grid
+            self.diff_grid[key]['actions'] = np.subtract(
+                self.initial_grid[key]['actions'], value['actions'])
 
     def make_maps(self):
+        """Creates intermediate grids by removing a single barrier one at
+        a time. Can randomize the process to create different grid
+        trajectories.
+        """
         new_map = copy.deepcopy(self.final_grid)
         other_key = ()
         barrier_cells = []
@@ -146,17 +192,43 @@ class GridWorld:
                         other_key = (x - 1, y)
                         other_look = 2
                 if other_key:
-                    self.update_grid(new_map, new_map, key, other_key, look, other_look)
+                    self.update_grid(new_map, new_map, key,
+                                     other_key, look, other_look)
                     self.list_of_maps.append(copy.deepcopy(new_map))
 
     def update_grid(self, c_map, new_map, key, other_key, look, other_look):
-        new_map[key]['actions'] = np.add(c_map[key]['actions'], np.eye(1, self.nA + 1, look)[0])
-        new_map[key]['actions'] = np.add(c_map[key]['actions'], self.diff_grid[key]['actions'])
-        new_map[other_key]['actions'] = np.add(new_map[other_key]['actions'], np.eye(1, self.nA + 1, other_look)[0])
-        self.diff_grid[key]['actions'] = np.subtract(self.diff_grid[key]['actions'], np.eye(1, self.nA + 1, look)[0])
-        self.diff_grid[other_key]['actions'] = np.subtract(self.diff_grid[other_key]['actions'], np.eye(1, self.nA + 1, other_look)[0])
+        """Updates the new map with the barrier removal change. Also removes
+        barriers from self.diff_grid for the next update so it is not repeated.
+
+        Args:
+            c_map (TYPE): current map
+            new_map (TYPE): updated map
+            key (TYPE): key of cell to be changed
+            other_key (TYPE): key of the adjacent cell to also be changed
+            look (TYPE): action direction. represents location of barrier
+                with respect to cell
+            other_look (TYPE): action direction of adjacent cell to be changed.
+        """
+        new_map[key]['actions'] = np.add(
+            c_map[key]['actions'], np.eye(1, self.nA + 1, look)[0])
+        new_map[key]['actions'] = np.add(
+            c_map[key]['actions'], self.diff_grid[key]['actions'])
+        new_map[other_key]['actions'] = np.add(
+            new_map[other_key]['actions'],
+            np.eye(1, self.nA + 1, other_look)[0])
+        self.diff_grid[key]['actions'] = np.subtract(
+            self.diff_grid[key]['actions'], np.eye(1, self.nA + 1, look)[0])
+        self.diff_grid[other_key]['actions'] = np.subtract(
+            self.diff_grid[other_key]['actions'],
+            np.eye(1, self.nA + 1, other_look)[0])
 
     def reset_state(self):
+        """Randomly chooses initial state on grid between four possible cells.
+        (0, 3), (0, 2), (1, 3), (1, 2)
+
+        Returns:
+            TYPE: Initial state
+        """
         p = np.random.random()
         if p < 0.25:
             return (self.x_start, self.y_start)
@@ -166,20 +238,3 @@ class GridWorld:
             return (self.x_start, self.y_start - 1)
         elif p < 1:
             return (self.x_start + 1, self.y_start - 1)
-
-# if __name__ == "__main__":
-
-    # grid = GridWorld()
-    # print("Initial Grid")
-    # grid.show_grid(grid.initial_grid)
-    # print("Final Grid")
-    # grid.show_grid(grid.final_grid)
-    # print("Differential Grid")
-    # grid.show_grid(grid.make_diff_grid())
-    # print("Multiple Grids In-between")
-    # grid.initial_grid
-    # grid.make_maps()
-    # for c_map in grid.list_of_maps:
-    #     grid.show_grid(c_map)
-
-    # print(len(grid.list_of_maps))
--- sarsa.py
+++ sarsa.py
@@ -1,3 +1,5 @@
+"""Applies SARSA algorithm to grid-world environments.
+"""
 import sys
 import numpy as np
 import itertools
@@ -7,117 +9,185 @@ import matplotlib.pyplot as plt
 from collections import defaultdict
 from grid_world import GridWorld
 
+
 class SARSA:
 
-	def __init__(self, c_map, possible_actions, x_lim, y_lim):
-		self.alpha = 0.5
-		self.discount_factor = 0.9
-		self.epsilon = 0.1
+    """Creates value and policy containers for SARSA evaluation of a
+    grid-world instance.
 
-		self.c_map = c_map
-		self.possible_actions = possible_actions
-		self.x_lim = x_lim
-		self.y_lim = y_lim
+    Attributes:
+        alpha (float): learning rate
+        c_map (TYPE): grid-world instance
+        discount_factor (float): discount factor
+        epsilon (float): policy choice probability
+        policy (TYPE): action policy
+        possible_actions (TYPE): possible actions for an agent to take
+        Q (TYPE): state action value storage
+        x_lim (TYPE): grid x-length
+        y_lim (TYPE): grid y-length
+    """
 
-		self.Q = dict()
-		self.policy = dict()
+    def __init__(self, c_map, possible_actions, x_lim, y_lim):
+        """Initializes SARSA parameters
 
-	def epsilon_greedy_random_action(self, state):
-		p = np.random.random()
-		count = 0
-		if p < self.epsilon:
-			action = random.choice(self.possible_actions)
-		else:
-			q_all = [self.Q.get((state, a), 0.0) for a in self.possible_actions]
-			max_a = [a for a in self.possible_actions if q_all[a] == max(q_all)]
-			if len(max_a) > 1:
-				action = random.choice(max_a)
-			else:
-				action = max_a[0]
-		self.policy[state] = action
-		return action
+        Args:
+            c_map (TYPE): grid-world instance
+            possible_actions (TYPE): possible actions for an agent to take
+            x_lim (TYPE): grid x-length
+            y_lim (TYPE): grid y-length
+        """
+        self.alpha = 0.5
+        self.discount_factor = 0.9
+        self.epsilon = 0.1
 
-	def update_Q(self, state, action, new_state, new_action, reward):
-		if self.Q.get((state, action), None):
-			q = self.Q[(state), action]
-			self.Q[state, action] = q + self.alpha * (reward + self.discount_factor * self.Q.get((new_state, action), 0.0) - q)
-		else:
-			self.Q[state, action] = reward
+        self.c_map = c_map
+        self.possible_actions = possible_actions
+        self.x_lim = x_lim
+        self.y_lim = y_lim
 
-	def take_step(self, state, action):
-		# from action [U, D, R, L] to state
-		x, y = state[0], state[1]
-		if self.c_map[state]['actions'][action]:
-				if action == 0:
-					new_state = (x, y + 1)
-				if action == 1:
-					new_state = (x, y - 1)
-				if action == 2:
-					new_state = (x + 1, y)
-				if action == 3:
-					new_state = (x - 1, y)
-		else:
-			new_state = state
-		return new_state, self.c_map[new_state]['reward']
+        self.Q = dict()
+        self.policy = dict()
 
-	def print_values(self):
-		for  y in range(self.y_lim-1, -1, -1):
-			print('-'*self.x_lim*9)
-			for x in range(self.x_lim):
-				value = max([self.Q.get(((x, y), a), 0.0) for a in self.possible_actions])
-				if value >= 0:
-					print(' {:.4f}'.format(value), "|", end="")
-				else:
-					print('{:.4f}'.format(value), "|", end="")
-			print("")
-		print('-'*self.x_lim*9)
+    def epsilon_greedy_random_action(self, state):
+        """Chooses action based on a greedy policy, but allows for
+        exploration by a 1 - epsilon probability.
+
+        Args:
+            state (TYPE): current state
+
+        Returns:
+            TYPE: action to be taken
+        """
+        p = np.random.random()
+        count = 0
+        if p < self.epsilon:
+            action = random.choice(self.possible_actions)
+        else:
+            q_all = [self.Q.get((state, a), 0.0)
+                     for a in self.possible_actions]
+            max_a = [a for a in self.possible_actions if q_all[
+                a] == max(q_all)]
+            if len(max_a) > 1:
+                action = random.choice(max_a)
+            else:
+                action = max_a[0]
+        self.policy[state] = action
+        return action
+
+    def update_Q(self, state, action, new_state, new_action, reward):
+        """Updates state value function
+
+        Args:
+            state (TYPE): current state
+            action (TYPE): action taken from current state
+            new_state (TYPE): new state
+            new_action (TYPE): action taken from new state
+            reward (TYPE): reward received upon taking an action from
+                the current state
+        """
+        if self.Q.get((state, action), None):
+            q = self.Q[(state), action]
+            self.Q[state, action] = q + self.alpha * \
+                (reward + self.discount_factor *
+                 self.Q.get((new_state, action), 0.0) - q)
+        else:
+            self.Q[state, action] = reward
+
+    def take_step(self, state, action):
+        """Agent performs action to transition to next state on grid-world.
+
+        Args:
+            state (TYPE): current state
+            action (TYPE): action taken from current state
+
+        Returns:
+            TYPE: new state and reward received from said state
+        """
+        # from action [U, D, R, L] to state
+        x, y = state[0], state[1]
+        if self.c_map[state]['actions'][action]:
+            if action == 0:
+                new_state = (x, y + 1)
+            if action == 1:
+                new_state = (x, y - 1)
+            if action == 2:
+                new_state = (x + 1, y)
+            if action == 3:
+                new_state = (x - 1, y)
+        else:
+            new_state = state
+        return new_state, self.c_map[new_state]['reward']
+
+    def print_values(self):
+        """Print state value function to terminal
+        """
+        for y in range(self.y_lim - 1, -1, -1):
+            print('-' * self.x_lim * 9)
+            for x in range(self.x_lim):
+                value = max([self.Q.get(((x, y), a), 0.0)
+                             for a in self.possible_actions])
+                if value >= 0:
+                    print(' {:.4f}'.format(value), "|", end="")
+                else:
+                    print('{:.4f}'.format(value), "|", end="")
+            print("")
+        print('-' * self.x_lim * 9)
+
 
 if __name__ == "__main__":
 
-	nEp = 200
-	grid = GridWorld()
-	grid.make_maps()
-	current_map = grid.list_of_maps[0] # Change index to get different maps 0-4
-	
-	if not current_map:
-		print("Map index is out of range.")
-		sys.exit()
+    # number of maximum episodes to run
+    nEp = 200
 
-	possible_actions = grid.possible_actions
-	x_lim, y_lim = grid.x_lim, grid.y_lim
+    # create grid-world instance
+    grid = GridWorld()
+    grid.make_maps()
 
-	sarsa = SARSA(current_map, possible_actions, x_lim, y_lim)
-	old_mean = 0
-	delta = 0.000001
-	steps = 0
+    # Change index to get different maps 0-4
+    current_map = grid.list_of_maps[0]
 
-	state = grid.reset_state()
-	print("Started at ", state)
-	for episode in range(nEp):
-		action = sarsa.epsilon_greedy_random_action(state)
-		for step in itertools.count():
-			new_state, reward = sarsa.take_step(state, action)
-			new_action = sarsa.epsilon_greedy_random_action(new_state)
-			sarsa.update_Q(state, action, new_state, new_action, reward)
+    if not current_map:
+        print("Map index is out of range.")
+        sys.exit()
 
-			if sarsa.c_map[new_state]['done']:
-				steps += step
-				break
-			else:
-				state, action = new_state, new_action
-		
-		current_mean = abs(np.mean(list(np.sum(sarsa.Q.values()))))
-		if np.abs(old_mean - current_mean) < delta:
-			print("Convergence at episode ", episode)
-			print("Number of steps ", steps)
-			break
-		else:
-			old_mean = current_mean
+    possible_actions = grid.possible_actions
+    x_lim, y_lim = grid.x_lim, grid.y_lim
 
-	print("Environment Map")
-	grid.show_grid(sarsa.c_map)
-	print("Environment Values")
-	sarsa.print_values()
-	print("Environment Policy")
-	grid.show_policy(sarsa.policy)
+    # creates SARSA instance
+    sarsa = SARSA(current_map, possible_actions, x_lim, y_lim)
 
+    # initialize algorithm parameters
+    old_mean = 0
+    delta = 0.000001
+    steps = 0
+
+    state = grid.reset_state()
+    print("Started at ", state)
+    for episode in range(nEp):
+        action = sarsa.epsilon_greedy_random_action(state)
+        for step in itertools.count():
+            new_state, reward = sarsa.take_step(state, action)
+            new_action = sarsa.epsilon_greedy_random_action(new_state)
+            sarsa.update_Q(state, action, new_state, new_action, reward)
+
+            if sarsa.c_map[new_state]['done']:
+                steps += step
+                break
+            else:
+                state, action = new_state, new_action
+
+        current_mean = abs(np.mean(list(np.sum(sarsa.Q.values()))))
+        if np.abs(old_mean - current_mean) < delta:
+            print("Convergence at episode ", episode)
+            print("Number of steps ", steps)
+            break
+        else:
+            old_mean = current_mean
+
+    # print results to terminal
+    print("Environment Map")
+    grid.show_grid(sarsa.c_map)
+    print("Environment Values")
+    sarsa.print_values()
+    print("Environment Policy")
+    grid.show_policy(sarsa.policy)
